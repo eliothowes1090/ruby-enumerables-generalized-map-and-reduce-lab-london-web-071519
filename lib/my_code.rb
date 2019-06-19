ec27a3569df2a323e795c3982f3205390ae23c55
Eliot Howes <ejhowes@gmail.com>
2019-06-19 21:00:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,15 +12,35 @@ def map(array)
 end
 
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, starting_point = nil)
   counter = 0
-  value = starting_point
-  memo = 0
 
+  if starting_point
+    memo = starting_point
+    counter = 0
+  else
+    memo = source_arrayp[0]
+    counter = 1
+  end
   while counter < source_array.length do
 
     value += yield(memo, source_array[counter])
     counter += 1
   end
   value
+end
+
+reduce(s, sp=nil)
+if sp
+  accum = sp
+  i = 0
+else
+  accum = s[0]
+  i = 1
+end
+while i < s.length
+  accum = yield(accum, s[i])
+  i += 1
+end
+accum
 end
