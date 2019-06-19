137d7b4f60126d73f1145cb6a67b4cbabeb53e27
Eliot Howes <ejhowes@gmail.com>
2019-06-19 21:01:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,31 +16,18 @@ def reduce(source_array, starting_point = nil)
   counter = 0
 
   if starting_point
-    memo = starting_point
+    value = starting_point
     counter = 0
   else
-    memo = source_arrayp[0]
+    value = source_arrayp[0]
     counter = 1
   end
-  while counter < source_array.length do
+  
+  while counter < source_array.length
 
-    value += yield(memo, source_array[counter])
+    value = yield(value, source_array[counter])
     counter += 1
   end
   value
 end
 
-reduce(s, sp=nil)
-if sp
-  accum = sp
-  i = 0
-else
-  accum = s[0]
-  i = 1
-end
-while i < s.length
-  accum = yield(accum, s[i])
-  i += 1
-end
-accum
-end
